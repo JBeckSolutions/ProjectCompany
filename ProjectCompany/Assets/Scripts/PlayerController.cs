8a68b7d9e89a9000a9dd875e86172426bc6e8c2f
Jonas Beck <jonas.beck552@gmail.com>
2025-03-24 14:59:07 +0100

    Added ability to jump
    Rewrote the Player Control script to use a CharacterController and added the ability to jump.

--- ProjectCompany/Assets/Scripts/PlayerController.cs
+++ ProjectCompany/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,25 @@ using UnityEngine.Jobs;
 
 public class PlayerController : NetworkBehaviour
 {
-    public float MovementSpeed = 4f;
-    private Vector2 moveInput;
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float jumpHeight = 1f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private CharacterController characterController;
 
+    private Vector2 moveInput;
+    private Vector3 playerVelocity;
+    private bool jumpThisFrame = false;
+
+    private void Update()
+    {
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
+        if (characterController.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+    }
+   
     //Disable PlayerInput component the other Player gameobjects to avoid conflicts
     public override void OnNetworkSpawn()
     {
@@ -16,11 +32,19 @@ public class PlayerController : NetworkBehaviour
     //Move when the Move input event is triggered
     public void OnMove(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        moveInput = context.ReadValue<Vector2>().normalized;
+        playerVelocity.x = moveInput.x;
+        playerVelocity.z = moveInput.y;
         //Debug.Log("Moving: " + moveInput);
     }
-    private void FixedUpdate()
+    public void OnJump(InputAction.CallbackContext context)
     {
-        this.transform.position += new Vector3(moveInput.x, 0, moveInput.y) * Time.deltaTime * MovementSpeed;
+        if (characterController.isGrounded && context.performed)
+        {
+            playerVelocity.y = 0;
+            jumpThisFrame = true;
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+        }
     }
+
 }
