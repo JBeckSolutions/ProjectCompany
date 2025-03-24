317ac7c5f22be3e875ef6becde4dc6d85d42ebff
Jonas Beck <jonas.beck552@gmail.com>
2025-03-24 16:07:09 +0100

    Added basic first person camera controls

--- ProjectCompany/Assets/Scripts/PlayerController.cs
+++ ProjectCompany/Assets/Scripts/PlayerController.cs
@@ -8,43 +8,77 @@ public class PlayerController : NetworkBehaviour
     [SerializeField] private float movementSpeed = 4f;
     [SerializeField] private float jumpHeight = 1f;
     [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float sensitivity = 0.2f;
     [SerializeField] private CharacterController characterController;
 
-    private Vector2 moveInput;
-    private Vector3 playerVelocity;
-    private bool jumpThisFrame = false;
+    [SerializeField] private GameObject playerCamera;
 
+    private Vector2 moveInput;
+    private float yAxisVelocity;
+    private Vector2 lookInput;
+    private float xRotation = 0;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            gameObject.GetComponent<PlayerInput>().enabled = false;
+            playerCamera.SetActive(false);
+        }
+    }
     private void Update()
     {
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        characterController.Move(playerVelocity * Time.deltaTime);
-        if (characterController.isGrounded && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+        HandleLook();
+        HandleMovement();
     }
    
     //Disable PlayerInput component the other Player gameobjects to avoid conflicts
-    public override void OnNetworkSpawn()
+    private void HandleLook()
     {
-        if (!IsOwner) gameObject.GetComponent<PlayerInput>().enabled = false;
+        float mouseX = lookInput.x * sensitivity;
+        float mouseY = lookInput.y * sensitivity;
+
+        // Rotate up/down (clamping to prevent flipping)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Rotate left/right
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 forward = playerCamera.transform.forward;
+        Vector3 right = playerCamera.transform.right;
+
+        Vector3 moveDirection = (forward.normalized * moveInput.y) + (right.normalized * moveInput.x);
+        yAxisVelocity += gravityValue * Time.deltaTime;
+        moveDirection.y = yAxisVelocity;
+        characterController.Move(moveDirection * Time.deltaTime);
+        if (characterController.isGrounded && yAxisVelocity < 0)
+        {
+            yAxisVelocity = 0f;
+        }
     }
     //Move when the Move input event is triggered
     public void OnMove(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>().normalized;
-        playerVelocity.x = moveInput.x;
-        playerVelocity.z = moveInput.y;
-        //Debug.Log("Moving: " + moveInput);
+        moveInput = context.ReadValue<Vector2>().normalized * movementSpeed;
     }
     public void OnJump(InputAction.CallbackContext context)
     {
         if (characterController.isGrounded && context.performed)
         {
-            playerVelocity.y = 0;
-            jumpThisFrame = true;
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+            yAxisVelocity = 0;
+            yAxisVelocity += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+        //Debug.Log(lookInput);
     }
 
 }
