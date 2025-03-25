39ca8787d66a284e9d72399d51252da07c27129e
Jonas Beck <jonas.beck552@gmail.com>
2025-03-25 14:26:55 +0100

    Added the ability to sprint

--- ProjectCompany/Assets/Scripts/PlayerController.cs
+++ ProjectCompany/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine.Jobs;
 public class PlayerController : NetworkBehaviour
 {
     [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float sprintSpeed = 6f;
     [SerializeField] private float jumpHeight = 1f;
     [SerializeField] private float gravityValue = -9.81f;
     [SerializeField] private float sensitivity = 0.2f;
@@ -18,12 +19,16 @@ public class PlayerController : NetworkBehaviour
     private Vector2 lookInput;
     private float xRotation = 0;
 
+    private bool sprinting = false;
+
     public override void OnNetworkSpawn()
     {
         if (!IsOwner)
         {
             gameObject.GetComponent<PlayerInput>().enabled = false;
             playerCamera.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
     private void Update()
@@ -32,7 +37,7 @@ public class PlayerController : NetworkBehaviour
         HandleMovement();
     }
    
-    //Disable PlayerInput component the other Player gameobjects to avoid conflicts
+    //Handles Rotation based on the input of the player
     private void HandleLook()
     {
         float mouseX = lookInput.x * sensitivity;
@@ -46,13 +51,23 @@ public class PlayerController : NetworkBehaviour
         // Rotate left/right
         transform.Rotate(Vector3.up * mouseX);
     }
-
+    //Handles Movement based on the input of the player
     private void HandleMovement()
     {
         Vector3 forward = playerCamera.transform.forward;
         Vector3 right = playerCamera.transform.right;
+        Vector2 _moveInput = moveInput; //Helper variable so moveInput doesnt multiply infinitly
 
-        Vector3 moveDirection = (forward.normalized * moveInput.y) + (right.normalized * moveInput.x);
+        if (sprinting)
+        {
+            _moveInput *= sprintSpeed;
+        }
+        else
+        {
+            _moveInput *= movementSpeed;
+        }
+
+        Vector3 moveDirection = (forward.normalized * _moveInput.y) + (right.normalized * _moveInput.x);
         yAxisVelocity += gravityValue * Time.deltaTime;
         moveDirection.y = yAxisVelocity;
         characterController.Move(moveDirection * Time.deltaTime);
@@ -61,10 +76,21 @@ public class PlayerController : NetworkBehaviour
             yAxisVelocity = 0f;
         }
     }
-    //Move when the Move input event is triggered
     public void OnMove(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>().normalized * movementSpeed;
+            moveInput = context.ReadValue<Vector2>().normalized;
+    }
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (characterController.isGrounded && context.performed)
+        {
+            sprinting = true;
+        }
+
+        if (context.canceled)
+        {
+            sprinting = false;
+        }
     }
     public void OnJump(InputAction.CallbackContext context)
     {
@@ -74,11 +100,27 @@ public class PlayerController : NetworkBehaviour
             yAxisVelocity += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
     }
-
     public void OnLook(InputAction.CallbackContext context)
     {
         lookInput = context.ReadValue<Vector2>();
         //Debug.Log(lookInput);
     }
+    public void OnOpenMenu(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+    
 
 }
