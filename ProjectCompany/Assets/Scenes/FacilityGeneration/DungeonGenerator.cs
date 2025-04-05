a5636206481d410de1255ef4ec1734789cdbe15a
Jonas Beck <jonas.beck552@gmail.com>
2025-04-06 00:15:06 +0200

    Lvl testing Scripts

---
+++ ProjectCompany/Assets/Scenes/FacilityGeneration/DungeonGenerator.cs
@@ -0,0 +1,94 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> roomPrefabs;
+    [SerializeField] private GameObject startingRoomPrefab;
+
+    [SerializeField] private int maxRooms;
+
+    [SerializeField] private bool useRandomSeed = true;
+    [SerializeField] private int seed = 0;
+
+    [SerializeField] private List<Room> placedRooms = new List<Room>();
+    [SerializeField] private List<Transform> availableConnections = new List<Transform>();
+
+    private void Start()
+    {
+        GenerateDungeon();
+    }
+
+    public void GenerateDungeon()
+    {
+        if (useRandomSeed)
+        {
+            seed = Random.Range(int.MinValue, int.MaxValue);
+        }
+
+        Random.InitState(seed);
+        Debug.Log("Dungeon Seed: " + seed);
+
+        GameObject start = Instantiate(startingRoomPrefab, Vector3.zero, Quaternion.identity);
+        Room startRoom = start.GetComponent<Room>();
+        placedRooms.Add(startRoom);
+        availableConnections.AddRange(startRoom.connectionPoints);
+
+        for (int i = 0; i < maxRooms; i++)
+        {
+            if (availableConnections.Count == 0) break;
+
+            Transform connectionPoint = availableConnections[Random.Range(0, availableConnections.Count)];
+            GameObject roomPrefab = roomPrefabs[Random.Range(0, roomPrefabs.Count)];
+            GameObject newRoom = Instantiate(roomPrefab);
+
+            Room roomComponent = newRoom.GetComponent<Room>();
+            if (roomComponent.connectionPoints.Count == 0)
+            {
+                Destroy(newRoom);
+                continue;
+            }
+
+            Transform newRoomEntrance = roomComponent.connectionPoints[0];
+            Quaternion targetRotation = Quaternion.LookRotation(connectionPoint.forward, Vector3.up);
+            newRoom.transform.rotation = targetRotation;
+            Vector3 offset = newRoom.transform.position - newRoomEntrance.transform.position;
+            newRoom.transform.position = connectionPoint.position;
+            newRoom.transform.localPosition += offset;
+
+            if (IsOverlapping(newRoom))
+            {
+                Destroy(newRoom);
+                continue;
+            }
+
+            placedRooms.Add(roomComponent);
+            availableConnections.AddRange(roomComponent.connectionPoints);
+            availableConnections.Remove(connectionPoint);
+            availableConnections.Remove(newRoomEntrance);
+        }
+
+    }
+
+    private bool IsOverlapping(GameObject room)
+    {
+        Collider[] colliders = Physics.OverlapBox(
+            room.transform.position,
+            room.GetComponent<BoxCollider>().size / 2,
+            room.transform.rotation);
+
+        foreach (var Collider in colliders)
+        {
+            if (Collider.isTrigger)
+            {
+                if (Collider.transform != room.transform)
+                {
+                    Debug.Log(Collider.transform.name);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ ProjectCompany/Assets/Scenes/FacilityGeneration/Room.cs
@@ -0,0 +1,8 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public List<Transform> connectionPoints = new List<Transform>();
+}
