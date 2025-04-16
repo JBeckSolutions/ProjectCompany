4baa86aa5d7dbf15f30c0837cba9d86770ae8129
AlexDosch <alexdosch18@gmail.com>
2025-04-16 17:18:10 +0200

    Menu + Inventory
    Added new Scene with Menu and new Inventory
    First draft for non base enemies

--- ProjectCompany/Assets/Scripts/Enemies/EnemyBase.cs
+++ ProjectCompany/Assets/Scripts/Enemies/EnemyBase.cs
@@ -60,7 +60,12 @@ public class EnemyBase : NetworkBehaviour
         playerList = GameManager.Singelton.PlayerStates;
     }
 
-    protected virtual void Update()
+    protected virtual float GetChaseSpeed()
+    {
+        return sprintSpeed;
+    }
+
+    public virtual void Update()
     {
         if (!IsServer) return;
 
@@ -91,7 +96,7 @@ public class EnemyBase : NetworkBehaviour
         {
             if (playerSeenThisFrame && player != null)
             {
-                agent.speed = sprintSpeed;  // Set speed to sprint when player is chased
+                agent.speed = GetChaseSpeed();  // Set speed to sprint when player is chased
                 ChooseNewDestination(player.transform.position);
                 if (timeUntilNextAttack <= 0 && Vector3.Distance(transform.position, player.transform.position) < attackRange)
                 {
--- ProjectCompany/Assets/Scripts/Player/PlayerInventoryManager.cs
+++ ProjectCompany/Assets/Scripts/Player/PlayerInventoryManager.cs
@@ -2,33 +2,79 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerInventoryManager : MonoBehaviour
 {
     [SerializeField] private Item[] inventoryItems;
-    [SerializeField] private RawImage[] inventroyImages;
-    [SerializeField] private RawImage[] activeInventroySlot;
-    [SerializeField] private int InventorySpace = 3;
+    //[SerializeField] private RawImage[] inventroyImages;
+    //[SerializeField] private RawImage[] activeInventroySlot;
+    [SerializeField] private int InventorySpace = 7;
     [SerializeField] private int ActiveInvetorySlot = 0;
     
     public Transform playerHand;
     [SerializeField] private Transform dropLocation;
 
+    private StyleColor activeColor = Color.blue;
+    private StyleColor oldColor;
+    private VisualElement[] slots;
+    private UnityEngine.UIElements.StyleBackground defaultImage;
+
     private void Start()
     {
-        inventoryItems = new Item[InventorySpace];
-        inventroyImages = new RawImage[InventorySpace];
-        activeInventroySlot = new RawImage[InventorySpace];
-        
+        inventoryItems = new Item[InventorySpace-1];
+        slots = new VisualElement[InventorySpace - 1];
+        //inventroyImages = new RawImage[InventorySpace-1];
+        //activeInventroySlot = new RawImage[InventorySpace - 1];
+
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        slots[0] = root.Q<VisualElement>("Slot1");
+        slots[1] = root.Q<VisualElement>("Slot2");
+        slots[2] = root.Q<VisualElement>("Slot3");
+        slots[3] = root.Q<VisualElement>("Slot4");
+        slots[4] = root.Q<VisualElement>("Slot5");
+        slots[5] = root.Q<VisualElement>("Slot6");
+        slots[6] = root.Q<VisualElement>("Slot7");
+
+        defaultImage = slots[0].style.backgroundImage;
+
+        oldColor = slots[0].style.color;
+        ChangeBorderColor(slots[0], true);
+        /*
         inventroyImages[0] = GameObject.Find("InventorySlotOne").GetComponent<RawImage>();
         inventroyImages[1] = GameObject.Find("InventorySlotTwo").GetComponent<RawImage>();
         inventroyImages[2] = GameObject.Find("InventorySlotThree").GetComponent<RawImage>();
         inventroyImages[3] = GameObject.Find("InventorySlotFour").GetComponent<RawImage>();
+        inventroyImages[4] = GameObject.Find("InventorySlotFive").GetComponent<RawImage>();
+        inventroyImages[5] = GameObject.Find("InventorySlotSix").GetComponent<RawImage>();
+        inventroyImages[6] = GameObject.Find("InventorySlotSeven").GetComponent<RawImage>();
 
         activeInventroySlot[0] = GameObject.Find("InventorySlotOneActive").GetComponent<RawImage>();
         activeInventroySlot[1] = GameObject.Find("InventorySlotTwoActive").GetComponent<RawImage>();
         activeInventroySlot[2] = GameObject.Find("InventorySlotThreeActive").GetComponent<RawImage>();
         activeInventroySlot[3] = GameObject.Find("InventorySlotFourActive").GetComponent<RawImage>();
+        activeInventroySlot[4] = GameObject.Find("InventorySlotFive").GetComponent<RawImage>();
+        activeInventroySlot[5] = GameObject.Find("InventorySlotSix").GetComponent<RawImage>();
+        activeInventroySlot[6] = GameObject.Find("InventorySlotSeven").GetComponent<RawImage>();
+        */
+    }
+
+    public void ChangeBorderColor(VisualElement slot, bool active)
+    {
+        if (active)
+        {
+            slot.style.borderBottomColor = activeColor;
+            slot.style.borderLeftColor = activeColor;
+            slot.style.borderRightColor = activeColor;
+            slot.style.borderTopColor = activeColor;
+        }
+        else
+        {
+            slot.style.borderBottomColor = oldColor;
+            slot.style.borderLeftColor = oldColor;
+            slot.style.borderRightColor = oldColor;
+            slot.style.borderTopColor = oldColor;
+        }     
     }
     public void AddItem(Item ItemToAdd)
     {
@@ -37,7 +83,7 @@ public class PlayerInventoryManager : MonoBehaviour
             if (ItemToAdd.PickupAble.Value == false) return;
             ItemToAdd.PickUpServerRpc(this.GetComponent<NetworkObject>());
             inventoryItems[ActiveInvetorySlot] = ItemToAdd;
-            inventroyImages[ActiveInvetorySlot].texture = ItemToAdd.InventoryImage;
+            slots[ActiveInvetorySlot].style.backgroundImage = ItemToAdd.InventoryImage;
         }
     }
     public void DropItem()
@@ -46,7 +92,7 @@ public class PlayerInventoryManager : MonoBehaviour
         {
             inventoryItems[ActiveInvetorySlot].DropServerRpc(dropLocation.position);
             inventoryItems[ActiveInvetorySlot] = null;
-            inventroyImages[ActiveInvetorySlot].texture = null;
+            slots[ActiveInvetorySlot].style.backgroundImage = defaultImage;
         }
     }
     public void ChangeActiveInventorySlot(int NewActiveInventorySlot)
@@ -56,11 +102,11 @@ public class PlayerInventoryManager : MonoBehaviour
             inventoryItems[ActiveInvetorySlot].ToggleVisibilityServerRpc(false);
         }
 
-        activeInventroySlot[ActiveInvetorySlot].enabled = false;
+        ChangeBorderColor(slots[ActiveInvetorySlot], false);
 
         ActiveInvetorySlot = NewActiveInventorySlot;   //Change active inventroy Slot
 
-        activeInventroySlot[ActiveInvetorySlot].enabled = true;
+        ChangeBorderColor(slots[ActiveInvetorySlot], true);
 
         if (ActiveInvetorySlot < 0)     //Check if slot is valid
         {
