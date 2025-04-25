d063defa838f3f27e6ab69dccb9e1ed28c60e6cf
Dennis Köhler <dennis.koehler@stud.hs-kempten.de>
2025-04-25 13:23:54 +0200

    Added Empty Soundbanks

--- ProjectCompany/Assets/Scripts/UI/MainMenu.cs
+++ ProjectCompany/Assets/Scripts/UI/MainMenu.cs
@@ -11,23 +11,32 @@ public class MainMenu : MonoBehaviour
         var root = GetComponent<UIDocument>().rootVisualElement;
 
         menuRoot = root.Q<VisualElement>("MainMenu");
-
+        
+        
+        
+        
         var newGameButton = root.Q<Button>("NewGameButton");
         newGameButton.clicked += () => {
             NetworkManager.Singleton.StartHost();
             menuRoot.style.display = DisplayStyle.None;
             FindFirstObjectByType<Interface>().EnableInventory();
+            
+            
         };
+        
+        
         var joinButton = root.Q<Button>("JoinButton");
         joinButton.clicked += () => {
             NetworkManager.Singleton.StartClient();
             menuRoot.style.display = DisplayStyle.None;
             FindFirstObjectByType<Interface>().EnableInventory();
         };
+        
         var settingsButton = root.Q<Button>("SettingsButton");
         settingsButton.clicked += () => {
 
         };
+        
         var quitButton = root.Q<Button>("QuitButton");
         quitButton.clicked += () => {
             Application.Quit();
