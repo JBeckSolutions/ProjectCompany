0cdc60ea92811d6dd8beec2d8892fe35f3ca853d
Dennis Köhler <dennis.koehler@stud.hs-kempten.de>
2025-04-25 14:27:56 +0200

    Added Sound Events for MainMenu_UI
    - Added MainMenu NewGame Click Event
    - Added MainMenu JoinGame Click Event
    - Added MainMenu Settings Click Event
    - Added MainMenu QuitGame Click Event
    
    - Added MainMenu NewGame Hover Event
    - Added MainMenu JoinGame Hover Event
    - Added MainMenu Settings Hover Event
    - Added MainMenu QuitGame Hover Event

--- ProjectCompany/Assets/Scripts/UI/MainMenu.cs
+++ ProjectCompany/Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,27 @@
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
     private VisualElement menuRoot;
+    
+    //Wwise Events from Inspector
+    [Header("Wwise MainMenu Button Click")]
+    public AK.Wwise.Event MainMenu_UI_Button_Click_NewGame;
+    public AK.Wwise.Event MainMenu_UI_Button_Click_JoinGame;
+    public AK.Wwise.Event MainMenu_UI_Button_Click_Settings;
+    public AK.Wwise.Event MainMenu_UI_Button_Click_QuitGame;
+    
+    [Header("Wwise MainMenu Button Hover")]
+    public AK.Wwise.Event MainMenu_UI_Button_Hover_NewGame; //to be added after HoverButton is implemented 
+    public AK.Wwise.Event MainMenu_UI_Button_Hover_JoinGame; //to be added after HoverButton is implemented 
+    public AK.Wwise.Event MainMenu_UI_Button_Hover_Settings; //to be added after HoverButton is implemented 
+    public AK.Wwise.Event MainMenu_UI_Button_Hover_QuitGame; //to be added after HoverButton is implemented 
+    
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,21 +29,18 @@ public class MainMenu : MonoBehaviour
 
         menuRoot = root.Q<VisualElement>("MainMenu");
         
-        
-        
-        
         var newGameButton = root.Q<Button>("NewGameButton");
         newGameButton.clicked += () => {
+            MainMenu_UI_Button_Click_NewGame.Post(gameObject);
             NetworkManager.Singleton.StartHost();
             menuRoot.style.display = DisplayStyle.None;
             FindFirstObjectByType<Interface>().EnableInventory();
-            
-            
         };
         
         
         var joinButton = root.Q<Button>("JoinButton");
         joinButton.clicked += () => {
+            MainMenu_UI_Button_Click_JoinGame.Post(gameObject);
             NetworkManager.Singleton.StartClient();
             menuRoot.style.display = DisplayStyle.None;
             FindFirstObjectByType<Interface>().EnableInventory();
@@ -34,11 +48,12 @@ public class MainMenu : MonoBehaviour
         
         var settingsButton = root.Q<Button>("SettingsButton");
         settingsButton.clicked += () => {
-
+            MainMenu_UI_Button_Click_Settings.Post(gameObject);
         };
         
         var quitButton = root.Q<Button>("QuitButton");
         quitButton.clicked += () => {
+            MainMenu_UI_Button_Click_QuitGame.Post(gameObject);
             Application.Quit();
         };
 
