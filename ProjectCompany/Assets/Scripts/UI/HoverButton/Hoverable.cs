d1ca85f2363afc360c059f59fd4c64b13104a637
Dennis Köhler <dennis.koehler@stud.hs-kempten.de>
2025-04-25 19:08:12 +0200

    Implemented Placeholder Sounds for Main Menu
    - Added Sound Events for MainMenu Buttons when Clicked
    - Added Sound Events for MainMenu Buttons when Hovered
    - Implemented HoverButton
    - Added Sounds on Mouse Pointer Hovering over Buttons
    - Added the supportive class Hoverable

---
+++ ProjectCompany/Assets/Scripts/UI/HoverButton/Hoverable.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine.UIElements;
+
+public class Hoverable : PointerManipulator
+{
+    public event Action hovered;
+
+    public Hoverable(Action hoverHandler)
+    {
+        hovered += hoverHandler;
+    }
+
+    protected override void RegisterCallbacksOnTarget()
+    {
+        target.RegisterCallback<MouseEnterEvent>(OnMouseEnter);
+    }
+
+    protected override void UnregisterCallbacksFromTarget()
+    {
+        target.UnregisterCallback<MouseEnterEvent>(OnMouseEnter);
+    }
+
+    private void OnMouseEnter(MouseEnterEvent evt)
+    {
+        hovered?.Invoke();
+    }
+}
+
--- ProjectCompany/Assets/Scripts/UI/MainMenu.cs
+++ ProjectCompany/Assets/Scripts/UI/MainMenu.cs
@@ -5,39 +5,61 @@ using UnityEngine.UIElements;
 public class MainMenu : MonoBehaviour
 {
     private VisualElement menuRoot;
+
+    //Wwise Events from Inspector
+    [Header("Wwise MainMenu Button Click")]
+    public AK.Wwise.Event MainMenu_UI_Button_Click_NewGame;
+    public AK.Wwise.Event MainMenu_UI_Button_Click_JoinGame;
+    public AK.Wwise.Event MainMenu_UI_Button_Click_Settings;
+    public AK.Wwise.Event MainMenu_UI_Button_Click_QuitGame;
+
+    [Header("Wwise MainMenu Button Hover")]
+    public AK.Wwise.Event MainMenu_UI_Button_Hover_NewGame;
+    public AK.Wwise.Event MainMenu_UI_Button_Hover_JoinGame;
+    public AK.Wwise.Event MainMenu_UI_Button_Hover_Settings;
+    public AK.Wwise.Event MainMenu_UI_Button_Hover_QuitGame;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
 
         menuRoot = root.Q<VisualElement>("MainMenu");
-
-        var newGameButton = root.Q<Button>("NewGameButton");
-        newGameButton.clicked += () => {
+        var newGameHoverButton = root.Q<HoverButton>("NewGameButton");
+        newGameHoverButton.clicked += () =>
+        {
+            MainMenu_UI_Button_Click_NewGame.Post(gameObject);
             NetworkManager.Singleton.StartHost();
             menuRoot.style.display = DisplayStyle.None;
             FindFirstObjectByType<Interface>().EnableInventory();
         };
-        var joinButton = root.Q<Button>("JoinButton");
-        joinButton.clicked += () => {
+        newGameHoverButton.hovered += () => { MainMenu_UI_Button_Hover_NewGame.Post(gameObject); };
+
+        var joinButton = root.Q<HoverButton>("JoinButton");
+        joinButton.clicked += () =>
+        {
+            MainMenu_UI_Button_Click_JoinGame.Post(gameObject);
             NetworkManager.Singleton.StartClient();
             menuRoot.style.display = DisplayStyle.None;
             FindFirstObjectByType<Interface>().EnableInventory();
         };
-        var settingsButton = root.Q<Button>("SettingsButton");
-        settingsButton.clicked += () => {
+        joinButton.hovered += () => { MainMenu_UI_Button_Hover_JoinGame.Post(gameObject); };
 
-        };
-        var quitButton = root.Q<Button>("QuitButton");
-        quitButton.clicked += () => {
+        var settingsButton = root.Q<HoverButton>("SettingsButton");
+        settingsButton.clicked += () => { MainMenu_UI_Button_Click_Settings.Post(gameObject); };
+        settingsButton.hovered += () => { MainMenu_UI_Button_Hover_Settings.Post(gameObject); };
+
+        var quitButton = root.Q<HoverButton>("QuitButton");
+        quitButton.clicked += () =>
+        {
+            MainMenu_UI_Button_Click_QuitGame.Post(gameObject);
             Application.Quit();
         };
-
+        quitButton.hovered += () => { MainMenu_UI_Button_Hover_QuitGame.Post(gameObject); };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
