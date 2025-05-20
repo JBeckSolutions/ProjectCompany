4147bdaa91dd52e82e779c532503c0b7eb8f657f
Jonas Beck <jonas.beck552@gmail.com>
2025-05-20 16:05:35 +0200

    Small fixes to allow the game to be build

--- ProjectCompany/Assets/Scripts/Player/PlayerController.cs
+++ ProjectCompany/Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Netcode;
-using UnityEngine.Jobs;
-using UnityEngine.Animations;
-using UnityEngine.UIElements;
 using Cursor = UnityEngine.Cursor;
-using static UnityEditor.Progress;
 
 public class PlayerController : NetworkBehaviour
 {
